9a26b47377bc6f9d080548bea48dc5f87fde4c48
dvir.monajem <dmenajem@gmail.com>
2025-07-06 21:45:33 +0300

    feat (app,.routes): add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/register/register').then(m => m.Register),
+  },
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./features/admin/admin').then(m => m.Admin),
+  },
+  {
+    path: '',
+    redirectTo: 'register',
+    pathMatch: 'full',
+  },
+];
