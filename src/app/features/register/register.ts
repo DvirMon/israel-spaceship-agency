4ca4ae9a8618a5f33930996672069dee4d668476
dvir.monajem <dmenajem@gmail.com>
2025-07-07 15:37:18 +0300

    feat (register): improve styling and add responsive stepper UI

--- src/app/features/register/register.ts
+++ src/app/features/register/register.ts
@@ -1,12 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, computed } from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
+import { MatStep, MatStepper } from "@angular/material/stepper";
+import { isMobile } from "../../utils/utils";
 @Component({
-  selector: 'app-register',
-  imports: [],
-  templateUrl: './register.html',
-  styleUrl: './register.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "app-register",
+  imports: [MatStepper, MatStep, MatCardModule],
+  templateUrl: "./register.html",
+  styleUrl: "./register.css",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Register {
+  private readonly isMobile = isMobile();
 
+  readonly stepperOrientation = computed(() =>
+    this.isMobile() ? "vertical" : "horizontal"
+  );
 }
