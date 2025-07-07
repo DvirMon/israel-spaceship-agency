0e6b707f0eb6cd5af165ad8aac53a2e79bf785fa
dvir.monajem <dmenajem@gmail.com>
2025-07-07 17:46:18 +0300

    ref (personal-info):  add pattern validation for name

--- src/app/features/register/utils/form.ts
+++ src/app/features/register/utils/form.ts
@@ -9,13 +9,17 @@ import {
 export function createPersonalInfoForm() {
   const nfb = inject(NonNullableFormBuilder);
   return nfb.group({
-    fullName: nfb.control("", [Validators.required, Validators.minLength(2)]),
+    fullName: nfb.control("", [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.pattern(/^[a-zA-ZÀ-ÿ'.\- ]+$/),
+    ]),
     email: nfb.control("", [Validators.required, Validators.email]),
     phone: nfb.control("", [
       Validators.required,
       Validators.pattern(/^(05[02-68]\d{7}|\+9725[02-68]\d{7})$/),
     ]),
-    age: nfb.control("", [
+    age: nfb.control<number | null>(null, [
       Validators.required,
       Validators.min(18),
       Validators.max(99),
