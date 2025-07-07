6bc329c013697a9ea87c78272b91b40883a1a94c
dvir.monajem <dmenajem@gmail.com>
2025-07-07 16:22:33 +0300

    feat (form.ts): add util function for personal info form

---
+++ src/app/features/register/utils/form.ts
@@ -0,0 +1,25 @@
+import { inject } from "@angular/core";
+import {
+    AbstractControl,
+    NonNullableFormBuilder,
+    ValidationErrors,
+    Validators
+} from "@angular/forms";
+
+export function createPersonalInfoForm() {
+  const nfb = inject(NonNullableFormBuilder);
+  return nfb.group({
+    fullName: nfb.control("", [Validators.required, Validators.minLength(2)]),
+    email: nfb.control("", [Validators.required, Validators.email]),
+    phone: nfb.control("", [
+      Validators.required,
+      Validators.pattern(/^(05[02-68]\d{7}|\+9725[02-68]\d{7})$/),
+    ]),
+    age: nfb.control("", [
+      Validators.required,
+      Validators.min(18),
+      Validators.max(99),
+    ]),
+    city: nfb.control("", [Validators.required]),
+  });
+}
