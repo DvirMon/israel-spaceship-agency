e2aa3401c21c593b265f2e4fc5c299e31a9ee909
dvir.monajem <dmenajem@gmail.com>
2025-07-07 22:41:55 +0300

    feat (form.ts):  add additional step form

--- src/app/features/register/utils/form.ts
+++ src/app/features/register/utils/form.ts
@@ -1,10 +1,6 @@
 import { inject } from "@angular/core";
-import {
-    AbstractControl,
-    NonNullableFormBuilder,
-    ValidationErrors,
-    Validators
-} from "@angular/forms";
+import { NonNullableFormBuilder, Validators } from "@angular/forms";
+import { imageFileValidator } from "../../../shared/file-upload/file.upload.utils";
 
 export function createPersonalInfoForm() {
   const nfb = inject(NonNullableFormBuilder);
@@ -19,11 +15,23 @@ export function createPersonalInfoForm() {
       Validators.required,
       Validators.pattern(/^(05[02-68]\d{7}|\+9725[02-68]\d{7})$/),
     ]),
-    age: nfb.control<number | null>(null, [
+    age: nfb.control<number | undefined>(undefined, [
       Validators.required,
       Validators.min(18),
       Validators.max(99),
     ]),
     city: nfb.control("", [Validators.required]),
+  });
+}
+
+export function createAdditionalInfoForm() {
+  const fnb = inject(NonNullableFormBuilder);
+
+  return fnb.group({
+    hobbies: fnb.control(""),
+    motivation: fnb.control("djskdjsk"),
+    profileImage: fnb.control<File | undefined>(undefined, [
+      imageFileValidator(),
+    ]),
   });
 }
