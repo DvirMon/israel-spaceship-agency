64cf61294c09582a7910b1ae2cddf9a5f88c38b6
dvir.monajem <dmenajem@gmail.com>
2025-07-10 09:39:38 +0300

    feat(candidate-filters): add component

---
+++ src/app/features/dashboard/candidates/components/candidate-filters/candidate-filters.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-filters',
+  imports: [],
+  templateUrl: './candidate-filters.html',
+  styleUrl: './candidate-filters.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CandidateFilters {
+
+}
