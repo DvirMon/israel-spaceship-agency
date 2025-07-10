0a3e7c6ae0a3581aa9bcea41c8b1a378516161d5
dvir.monajem <dmenajem@gmail.com>
2025-07-10 08:37:22 +0300

    ref(geocode/service): add Map for caching

--- src/app/core/services/geocoding.service.ts
+++ src/app/core/services/geocoding.service.ts
@@ -1,18 +1,23 @@
-// geocoding.service.ts
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
-import { map, Observable } from "rxjs";
+import { map, of } from "rxjs";
 
 @Injectable({ providedIn: "root" })
 export class GeocodingService {
   private http = inject(HttpClient);
+  private geoCache = new Map<
+    string,
+    { lat: number; lng: number; formatted: string }
+  >();
 
   private readonly API_URL = "https://api.opencagedata.com/geocode/v1/json";
   private readonly API_KEY = "be345afe3c6c42afb33bd71a1a12a01e";
 
-  loadCoordinates(
-    city: string
-  ): Observable<{ lat: number; lng: number; formatted: string } | null> {
+  loadCoordinates(city: string) {
+    if (this.geoCache.has(city)) {
+      return of(this.geoCache.get(city)!); // ✅ return cached result
+    }
+
     const params = {
       q: city,
       key: this.API_KEY,
@@ -23,13 +28,16 @@ export class GeocodingService {
     return this.http.get<any>(this.API_URL, { params }).pipe(
       map((response) => {
         const result = response.results?.[0];
-        return result
+        const geo = result
           ? {
               lat: result.geometry.lat,
               lng: result.geometry.lng,
               formatted: result.formatted,
             }
-          : null;
+          : { lat: 0, lng: 0, formatted: "" };
+
+        this.geoCache.set(city, geo); // 💾 cache it
+        return geo;
       })
     );
   }
