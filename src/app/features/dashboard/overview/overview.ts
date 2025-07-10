4edb0a4c40c9df56b06ca32974ca0657d64c9cd1
dvir.monajem <dmenajem@gmail.com>
2025-07-10 14:50:16 +0300

    feat(overview): add charts to template

--- src/app/features/dashboard/overview/overview.ts
+++ src/app/features/dashboard/overview/overview.ts
@@ -1,12 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
+import { AgeChart } from "app/shared/components/age-chart/age-chart";
+import { ChartData, DashboardService } from "../dashboard.service";
 
 @Component({
-  selector: 'app-overview',
-  imports: [],
-  templateUrl: './overview.html',
-  styleUrl: './overview.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "app-overview",
+  imports: [MatCardModule, AgeChart],
+  templateUrl: "./overview.html",
+  styleUrl: "./overview.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Overview {
+  private readonly dashboardService = inject(DashboardService);
 
+  ageData = signal<ChartData[]>(this.dashboardService.getAgeData());
+  // locationData = signal<LocationData[]>(
+  //   this.dashboardService.getLocationData()
 }
