5aedcf28719a6cfbc65011f14f8f3bc7102d380f
dvir.monajem <dmenajem@gmail.com>
2025-07-13 10:53:51 +0300

    fix(environment): move geo api to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  geo_key: "be345afe3c6c42afb33bd71a1a12a01e",
   firebase: {
     projectId: "israel-spaceship-agency",
     appId: "1:802854334536:web:5a8c1877f328544ca3a87d",
