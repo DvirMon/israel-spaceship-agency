9ddf634dd205919635180f974667403a2e0b884a
dvir.monajem <dmenajem@gmail.com>
2025-07-16 07:38:18 +0300

    ref(map-location): load leaflet lazy to save runtime budget

--- src/app/core/leaflet/ provide-leaflet-map.ts
+++ src/app/core/leaflet/ provide-leaflet-map.ts
@@ -1,11 +1,9 @@
-// provide-leaflet-map.ts
-import { makeEnvironmentProviders, Provider } from "@angular/core";
-import { LeafletMap } from "./leaflet-map.service";
+import { makeEnvironmentProviders } from "@angular/core";
 import {
   LEAFLET_MAP_CONFIG,
   LeafletMapConfig,
 } from "./leaflet-map-config.token";
-import * as L from "leaflet";
+import { LeafletMap } from "./leaflet-map.service";
 
 export const DEFAULT_CONFIG: LeafletMapConfig = {
   zoom: 8,
@@ -16,15 +14,9 @@ export const DEFAULT_CONFIG: LeafletMapConfig = {
 export function provideLeafletMap(config: Partial<LeafletMapConfig> = {}) {
   const mergedConfig = { ...DEFAULT_CONFIG, ...config };
 
-  // Set global default marker icon
-  const defaultIcon = L.icon({
-    ...L.Icon.Default.prototype.options,
-    iconUrl: "assets/marker-icon.png",
-    iconRetinaUrl: "assets/marker-icon-2x.png",
-    shadowUrl: "assets/marker-shadow.png",
-  });
+  // // Set global default marker icon
 
-  L.Marker.prototype.options.icon = defaultIcon;
+
 
   return makeEnvironmentProviders([
     { provide: LEAFLET_MAP_CONFIG, useValue: mergedConfig },
